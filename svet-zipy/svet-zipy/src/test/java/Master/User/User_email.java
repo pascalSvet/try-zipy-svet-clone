cae86eef7cf6ba0ca6ec80e8b797918abd92d22f
Sveta <Sveta@DESKTOP-7OT8PUT>
2020-01-27 13:14:00 +0200

    update "user tests" - adding email and mobile tests

---
+++ svet-zipy/svet-zipy/src/test/java/Master/User/User_email.java
@@ -0,0 +1,45 @@
+package Master.User;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Scanner;
+import org.junit.experimental.runners.Enclosed;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import junit.framework.Assert;
+import zipy_elements.*;
+
+public class User_email extends User_MAIN {
+			
+		
+	//TEST - changing the email
+	@Test	
+	public  void user_changeEmail() throws Exception {
+		System.out.println("running test for changing the email");
+		
+		driver.get(ElementsWebsites.Zipy_il);
+
+		//enter private data and change password 
+		Functions.changeEmail(driver, ElementsLogin.Email_zipy2, ElementsLogin.Email_zipy_password);
+
+		//if changed correctly, there is a success message
+		Assert.assertFalse(driver.findElements(By.xpath(ElementsLogin.user_changePassword_succesMessageVi)).isEmpty());
+		
+		//change the email back at the end, for future tests
+		driver.get(ElementsWebsites.Zipy_il);
+		Functions.changeEmail(driver, ElementsLogin.Email_zipy, ElementsLogin.Email_zipy_password);
+	
+	}
+	
+}
